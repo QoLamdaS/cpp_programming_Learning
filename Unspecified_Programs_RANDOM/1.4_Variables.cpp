ecb0a325d91911fa6713294153b21a7e17d37743
QoLamdaS <kaffeeunused@gmail.com>
2025-07-02 14:32:01 +0700

    add(variables): Add some int and double variables for experimenting.

--- Unspecified_Programs_RANDOM/1.4_Variables.cpp
+++ Unspecified_Programs_RANDOM/1.4_Variables.cpp
@@ -8,6 +8,12 @@ int main()
     std::cout << age;
     age = 88888888;
     std::cout << "\n" << age;
+
+    int AgE_2 {232}; //! {232} is a INITIALIZER
+    std::cout << "\n" << AgE_2;
+    double PIE {314.444314};
+    std::cout << "\n" << PIE;
+    
     return 0;
 
 }
