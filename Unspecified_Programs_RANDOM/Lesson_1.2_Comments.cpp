c29fb631fb3caf085a8a742a653b913378a35c2d
QoLamdaS <kaffeeunused@gmail.com>
2025-07-02 12:57:12 +0700

    add(output): Add output print

--- Unspecified_Programs_RANDOM/Lesson_1.2_Comments.cpp
+++ Unspecified_Programs_RANDOM/Lesson_1.2_Comments.cpp
@@ -2,6 +2,12 @@
 
 int main() {
 
-    std::cout << "Hello, World!";
+    std::cout << "\nHello, World!\n";
+    std::cout << "Hello, World! VERSION 0222sds23\n";
+    std::cout << "Hello, World! version 3\n";
+    /* This is a multi-line comment.
+    Line 2
+    Third line */
+
     return 0;
 }
