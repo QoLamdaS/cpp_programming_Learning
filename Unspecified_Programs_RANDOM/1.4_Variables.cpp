f05bb4f9bed111f991c70d0645881e7ec727c11c
QoLamdaS <kaffeeunused@gmail.com>
2025-07-03 15:40:25 +0700

    add(attribute_cpp17): Add [[maybe_unused]] in C++ 17.

--- Unspecified_Programs_RANDOM/1.4_Variables.cpp
+++ Unspecified_Programs_RANDOM/1.4_Variables.cpp
@@ -11,12 +11,15 @@ int main()
 
     int AgE_2 {232}; //! {232} is a INITIALIZER
     std::cout << "\n" << AgE_2;
-    double PIE {314.444314};
+    double PIE {314.444314}; //* {} is only for initializer
     std::cout << "\n" << PIE;
     PIE = 2123.4533;
     std::cout << "\n" << PIE;
     PIE = 453923.456;
     std::cout << "\n" << PIE;
+
+    [[maybe_unused]] double gravity {9.8}; //* Don't complain if gravity is unused
+    //! The compiler will no longer warn about gravity not being used
     
     return 0;
 
