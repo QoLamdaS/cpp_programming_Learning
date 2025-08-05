bdeeccc23c43ff0b8bfffe4ba68cff8a20d80c01
QoLamdaS <kaffeeunused@gmail.com>
2025-08-05 21:21:27 +0700

    Idk

--- Unspecified_Programs_RANDOM/1.5_iostream.cpp
+++ Unspecified_Programs_RANDOM/1.5_iostream.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-#include <string> // Required for std::string
+#include <string> //Todo: Required for std::string
 //! FINALLY FIXED, VScode's Terminal worked, use G++ in "tasks.json" not gcc
 int main() {
     int age;
     std::cout << "Enter your age: ";
-    std::cin >> age; // Reads an integer into 'age'
+    std::cin >> age; //* Reads an integer into 'age'
 
     std::string firstName;
     std::cout << "Enter your first name: ";
-    std::cin >> firstName; // Reads a single word string into 'firstName'
+    std::cin >> firstName; //* Reads a single word string into 'firstName'
 
     std::cout << "Hello, " << firstName << "! You are " << age << " years old." << std::endl;
     return 0;
